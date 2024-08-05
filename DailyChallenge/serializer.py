f60218b3589cd8ade83fb5da4e9b4885fe75eee8
tr1b1iwnl <tr1b1iwnl@gmail.com>
2024-08-05 12:37:15 +0800

    Added questions to dailychallenges serializer

--- DailyChallenge/serializer.py
+++ DailyChallenge/serializer.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
+
+from Mocktest.serializer import MockQuestionsSerializer
 from .models import DailyChallenge, DailyChallengeQuestion, DailyChallengeLeaderboard
 
 
-class DailyChallengeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DailyChallenge
-        fields = '__all__'
-
-
 class DailyChallengeQuestionSerializer(serializers.ModelSerializer):
+    question = MockQuestionsSerializer()
+
     class Meta:
         model = DailyChallengeQuestion
-        fields = '__all__'
+        fields = ['id', 'question']
 
 
 class DailyChallengeLeaderboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyChallengeLeaderboard
         fields = '__all__'
+
+
+class DailyChallengeSerializer(serializers.ModelSerializer):
+    daily_challenge_questions = DailyChallengeQuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = DailyChallenge
+        fields = ['challengeID', 'date', 'daily_challenge_questions']
